69ec89e107854568de7c10269cd2d7e32bb7422a
Toshiaki Baba <toshiaki@netmark.jp>
2012-03-31 15:22:47 +0900

    add staticfiles implementation

--- setup.sh
+++ setup.sh
@@ -20,26 +20,39 @@ env ARCHFLAGS="-arch i386 -arch x86_64" bin/pip install psycopg2
 bin/pip install gunicorn
 bin/pip freeze > requirements.txt
 bin/django-admin.py startproject ${PROJECT_NAME}
-cat <<EOT | bin/python - ${PROJECT_NAME}/settings.py > tmpsettings.py
+echo 'import os'>tmpsettings.py
+echo 'PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))'>>tmpsettings.py
+cat <<EOT | bin/python - ${PROJECT_NAME}/settings.py >> tmpsettings.py
 import sys,re
 
 for line in open(sys.argv[1]):
+    line=re.sub(r'^STATIC_ROOT =.*','STATIC_ROOT = os.path.join(PROJECT_PATH, \'static\')',line)
+    line=re.sub(r'^STATIC_URL =.*','STATIC_URL = \'/static\'',line)
+    line=re.sub(r'^STATICFILES_DIRS = \(','STATICFILES_DIRS = (\n    os.path.join(PROJECT_PATH, \'staticfiles\'),',line)
     line=re.sub(r'^INSTALLED_APPS = \(','INSTALLED_APPS = (\n    \'gunicorn\',',line)
     line=re.sub(r'^TIME_ZONE = \'America/Chicago\'','TIME_ZONE = \'Asia/Tokyo\'',line)
     line=re.sub(r'^LANGUAGE_CODE = \'en-us\'','LANGUAGE_CODE = \'ja\'',line)
     sys.stdout.write(line)
 EOT
+cat <<EOT >> ${PROJECT_NAME}/urls.py
+from ${PROJECT_NAME} import settings
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
+EOT
 mv -f tmpsettings.py ${PROJECT_NAME}/settings.py
-echo "web: python ${PROJECT_NAME}/manage.py run_gunicorn -b 0.0.0.0:\$PORT" >Procfile
+echo "web: python sandbox/manage.py collectstatic --noinput; python sandbox/manage.py run_gunicorn -b 0.0.0.0:\$PORT" >Procfile
 cat <<EOT >.gitignore
 bin/
 include/
 lib/
 build/
+tmp/
 *.pyc
 *.swp
 .Python
 EOT
+mkdir ${PROJECT_NAME}/staticfiles
 git init
 git add .
 git commit -m 'initial import'
